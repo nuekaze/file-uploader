108fcb8bab352a7135259296ec084f52817e5d1d
Nuekaze <nue@airmail.cc>
2022-05-03 20:44:49 +0000

    Create server.py

---
+++ server.py
@@ -0,0 +1,52 @@
+from wsgiref.simple_server import make_server
+from cgi import FieldStorage
+
+html = '''
+<html>
+    <head>
+        <title>Upload file</title>
+    </head>
+    <body>
+        <h1>Upload file</h1>
+        <p>
+            <form method="post" enctype="multipart/form-data">
+                <label>File</label>
+                <input type="file" name="myfile">
+                <input type="submit" value="Send">
+            </form>
+        </p>
+        <p><!--RESULT--></p>
+    </body>
+</html>
+'''
+
+upload_dir = '/storage/tempfiles'
+port = 8000
+i = 0
+
+def app(environ, start_response):
+    global i
+    response = ''
+    
+    if environ['REQUEST_METHOD'].upper() == 'POST':
+        fs = FieldStorage(fp=environ['wsgi.input'], environ=environ,keep_blank_values=True)['myfile']
+        if fs.file:
+            name = str(i).rjust(3, '0') + '.' + fs.filename.split('.')[1:][0]
+            open(upload_dir + '/' + name, 'wb').write(fs.file.read())
+            response = html.replace('<!--RESULT-->', name)
+            if i == 999:
+                i = 0
+            else:
+                i += 1
+        else:
+            response = html.replace('<!--RESULT-->', 'No file.')
+
+    else:
+        response = html
+
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [bytes(response, 'utf-8')]
+
+print('Starting web server.')
+httpd = make_server('', port, app)
+httpd.serve_forever()
