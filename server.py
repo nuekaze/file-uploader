ea0846bf476a4f6dc894968052c466b9dd650010
Nuekaze <nue@airmail.cc>
2022-05-03 20:57:50 +0000

    Update server.py

--- server.py
+++ server.py
@@ -32,10 +32,15 @@ def app(environ, start_response):
     
     if environ['REQUEST_METHOD'].upper() == 'POST':
         fs = FieldStorage(fp=environ['wsgi.input'], environ=environ,keep_blank_values=True)['myfile']
+        
         if fs.file:
+            # Just some temporary name using a counter.
             name = str(i).rjust(3, '0') + '.' + fs.filename.split('.')[1:][0]
+            
             open(upload_dir + '/' + name, 'wb').write(fs.file.read())
             response = html.replace('<!--RESULT-->', website + '/' + name)
+            
+            # Increase counter.
             if i == 999:
                 i = 0
             else:
